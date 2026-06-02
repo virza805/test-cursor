4221f610e0ca7a20361797a8ede859fd6c1ba3b0
Tanvir Md. Al-Amin <01mdalamin1@gmail.com>
2026-06-02 23:40:37 +0600

    test create next.js 16

---
+++ nextjs_demo/app/about/page.tsx
@@ -0,0 +1,7 @@
+function AboutPage() {
+  return (
+    <div className="text-center text-amber-600 text-2xl">About Page</div>
+  )
+}
+
+export default AboutPage
---
+++ nextjs_demo/components/hello.tsx
@@ -0,0 +1,8 @@
+
+const Hello = () => {
+  return (
+    <div className="text-2xl text-green-700 text-center">hello</div>
+  )
+}
+
+export default Hello
